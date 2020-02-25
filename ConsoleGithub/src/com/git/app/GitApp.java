90ea7712d39fe909b43b16df6ebcdc420fce07e2
anniejojo <anniejojo21@gmail.com>
2020-02-25 17:24:50 +0530

    Second Commit

--- ConsoleGithub/src/com/git/app/GitApp.java
+++ ConsoleGithub/src/com/git/app/GitApp.java
@@ -4,8 +4,9 @@ public class GitApp {
 
 	public static void main(String[] args) {
 
-
+//to display the output
 			System.out.println("Welcome to our world....");
+			System.out.println("Message");
 
 	}
 
